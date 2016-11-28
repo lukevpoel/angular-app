db39e7e3740b783aa9577054ac309ddc11c6570c
Luke Vander Poel <airscout99@gmail.com>
2016-11-27 19:52:36 -0800

    updated with basic trans and msgs

--- app/scripts/controllers/current.js
+++ app/scripts/controllers/current.js
@@ -27,16 +27,24 @@ angular.module('angularAppApp')
             var save = true; // Initialize the save decision variable.
             // Use this loop to check if we've already saved the city.
             for (var i=0; i < $localStorage.savedCities.length; i++){
-              if ($localStorage.savedCities[i].id == cityData.id) {
+              if ($localStorage.savedCities[i].id === cityData.id) {
                   // This is a duplicate, so don't save (variable set to false).
                   save = false;
                 }
             }
-            if (save==true){
+            if (save===true){
               $localStorage.savedCities.push(cityData);
               console.log('saving this city');
+              // Add object to trigger saved message
+              $scope.citySaved = {
+                'success' : true
+              };
             } else {
               console.log('city already saved');
+              // Add object to trigger message
+              $scope.citySaved = {
+                'duplicate' : true
+              };
             }
         }
     };
