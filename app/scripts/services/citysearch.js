624373980d5cec17c6afa8a05d18fe865821f27f
Luke Vander Poel <airscout99@gmail.com>
2016-12-04 23:33:22 -0800

    fixed homepage temps

--- app/scripts/services/citysearch.js
+++ app/scripts/services/citysearch.js
@@ -13,7 +13,7 @@ angular.module('angularAppApp')
       // ...
 
       // Public API here
-      return $resource('http://api.openweathermap.org/data/2.5/find?q=:query&type=like&mode=json&APPID=fdfc6704f679f2989e7602055aaff7e8', {}, {
+      return $resource('http://api.openweathermap.org/data/2.5/find?q=:query&type=like&units=imperial&mode=json&APPID=fdfc6704f679f2989e7602055aaff7e8', {}, {
         find: {
           method: 'GET',
           params: {
